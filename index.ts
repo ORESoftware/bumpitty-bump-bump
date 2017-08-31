86b89751d044cc026fd2899075b87f7f76853c19
Olegzandr VD <alex@oresoftware.com>
2017-08-31 15:34:08 -0700

    pdev:set

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import path = require('path');
 import residence = require('residence');
 import ErrnoException = NodeJS.ErrnoException;
 
-let syncSetup = function(){
+let syncSetup = function () {
   const projectRoot = residence.findProjectRoot(process.cwd());
   const pkgPath = path.resolve(projectRoot + '/package.json');
   const pkg = require(pkgPath);
@@ -14,26 +14,24 @@ let syncSetup = function(){
   return {pkg, pkgPath};
 };
 
-
-export const bumpSync = function(){
+export const bumpSync = function () {
   const {pkg, pkgPath} = syncSetup();
   fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
 };
 
-export const bumpp = function(){
-  return new Promise(function(resolve,reject){
+export const bumpp = function () {
+  return new Promise(function (resolve, reject) {
     const {pkg, pkgPath} = syncSetup();
-    fs.writeFile(pkgPath, JSON.stringify(pkg, null, 2), function(err){
-        err ? reject(err) : resolve();
+    fs.writeFile(pkgPath, JSON.stringify(pkg, null, 2), function (err) {
+      err ? reject(err) : resolve();
     });
   });
 };
 
-export const bump = function(cb: ErrnoException){
+export const bump = function (cb: ErrnoException) {
   const {pkg, pkgPath} = syncSetup();
   fs.writeFile(pkgPath, JSON.stringify(pkg, null, 2), cb);
 };
-
 
 if (require.main === module) {
   bumpSync();
